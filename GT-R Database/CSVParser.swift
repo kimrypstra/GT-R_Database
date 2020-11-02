32715efb9d0af7633f6400b81405522ce77321b6
Kim Rypstra <kim.rypstra@gmail.com>
2020-11-02 10:37:22 +0800

    Added struct for R34

---
+++ GT-R Database/CSVParser.swift
@@ -0,0 +1,8 @@
+//
+//  CSVParser.swift
+//  GT-R Database
+//
+//  Created by Kim Rypstra on 1/11/20.
+//
+
+import Foundation
---
+++ GT-R Database/DataStructures.swift
@@ -0,0 +1,21 @@
+//
+//  DataStructures.swift
+//  GT-R Database
+//
+//  Created by Kim Rypstra on 1/11/20.
+//
+
+import Foundation
+
+struct R34 {
+    let ID: String
+    let VIN: String
+    let Grade: String
+    let Series: String
+    let Colour: String
+    let Production: String
+    let Date: String
+    let Plant: String
+    let Seat: String
+    let Model: String
+}
